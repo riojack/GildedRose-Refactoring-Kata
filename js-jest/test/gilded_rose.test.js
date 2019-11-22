24dbb6bc8c5f97f8852d77e3a4929811d68d9744
riojack <ayjay122@gmail.com>
2019-11-22 09:16:18 -0600

    refactor: extract magic numbers in test

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -2,8 +2,12 @@ const {Shop, Item} = require('../src/gilded_rose');
 
 describe('Gilded Rose', function () {
   it('should foo', function () {
-    const gildedRose = new Shop([new Item('foo', 0, 0)]);
+    const sellIn = 0;
+    const quality = 0;
+    const gildedRose = new Shop([new Item('foo', sellIn, quality)]);
+
     const items = gildedRose.updateQuality();
+
     expect(items[0].name).toBe('foo');
   });
 });
