ed120a4ddfbc7db70f566349966a2fa92f704de1
riojack <ayjay122@gmail.com>
2019-11-22 16:13:59 -0600

    checkpoint

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -58,4 +58,16 @@ describe('Gilded Rose', () => {
 
     expect(shop.items[0].quality).toBe(6);
   });
+
+  it('should increase Aged Brie\'s quality by one even after it has expired', () => {
+    const sellIn = 0;
+    const quality = 5;
+
+    const brewfestSampler = new Item('Aged Brie', sellIn, quality);
+
+    const shop = new Shop([brewfestSampler]);
+    shop.updateQuality();
+
+    expect(shop.items[0].quality).toBe(6);
+  });
 });
