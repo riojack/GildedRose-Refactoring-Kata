973b2a051f1d00dfa2d2888d2204ad329510a647
riojack <ayjay122@gmail.com>
2019-11-22 09:19:31 -0600

    refactor: convert to arrow functions

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,7 +1,7 @@
 const {Shop, Item} = require('../src/gilded_rose');
 
-describe('Gilded Rose', function () {
-  it('should foo', function () {
+describe('Gilded Rose', () => {
+  it('should foo', () => {
     const sellIn = 0;
     const quality = 0;
     const gildedRose = new Shop([new Item('foo', sellIn, quality)]);
