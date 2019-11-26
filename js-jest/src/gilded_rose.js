0815fccf6b44dee47d16e7a515af2beda1363797
riojack <ayjay122@gmail.com>
2019-11-26 08:30:32 -0600

    refactor: extract quality update logic for brie and passes

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -2,6 +2,22 @@ const SULFURAS = 'Sulfuras, Hand of Ragnaros';
 const BRIE = 'Aged Brie';
 const PASSES = 'Backstage passes to a TAFKAL80ETC concert';
 
+const updateQualityForAgedBrieOrBackstagePasses = (item) => {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+
+    if (item.name == PASSES) {
+      if (item.sellIn < 11) {
+        item.quality = item.quality + 1;
+      }
+
+      if (item.sellIn < 6) {
+        item.quality = item.quality + 1;
+      }
+    }
+  }
+};
+
 class Shop {
   constructor(items = []) {
     this.items = items;
@@ -16,19 +32,7 @@ class Shop {
       }
 
       if (item.name == BRIE || item.name == PASSES) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-
-          if (item.name == PASSES) {
-            if (item.sellIn < 11) {
-              item.quality = item.quality + 1;
-            }
-
-            if (item.sellIn < 6) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
+        updateQualityForAgedBrieOrBackstagePasses(item);
       } else if (item.quality > 0) {
         item.quality = item.quality - 1;
       }
