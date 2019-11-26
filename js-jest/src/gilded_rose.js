efc25f2049cc7bb8f55d4de937a4e96e09f2454e
riojack <ayjay122@gmail.com>
2019-11-26 14:13:58 -0600

    refactor: use less-than-equals, for consistency

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -15,9 +15,9 @@ const updateAgedBrieOrBackstagePass = (item) => {
 
   if (item.sellIn === 0) {
     item.quality = 0;
-  } else if (item.sellIn < 6) {
+  } else if (item.sellIn <= 5) {
     item.quality = item.quality + 2;
-  } else if (item.sellIn < 11) {
+  } else if (item.sellIn <= 10) {
     item.quality = item.quality + 1;
   }
 };
