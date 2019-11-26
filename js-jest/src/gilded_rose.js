2a86b7bfcd31d5e07e67d2245c9a8d32add31072
riojack <ayjay122@gmail.com>
2019-11-26 13:38:53 -0600

    refactor: copying each item is a separate map() call

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -24,37 +24,37 @@ class Shop {
   }
 
   updateQuality() {
-    const updatedItems = this.items.map((item) => {
-      const updatedItem = item.copy();
+    const updatedItems = this.items
+      .map((item) => item.copy())
+      .map((item) => {
+        if (item.name == SULFURAS) {
+          return item;
+        }
 
-      if (updatedItem.name == SULFURAS) {
-        return updatedItem;
-      }
+        if (item.sellIn > 0) {
+          item.sellIn = item.sellIn - 1;
+        }
 
-      if (updatedItem.sellIn > 0) {
-        updatedItem.sellIn = updatedItem.sellIn - 1;
-      }
+        if (item.name == BRIE || item.name == PASSES) {
+          updateQualityForAgedBrieOrBackstagePasses(item);
+        } else if (item.quality > 0) {
+          item.quality = item.quality - 1;
+        }
 
-      if (updatedItem.name == BRIE || updatedItem.name == PASSES) {
-        updateQualityForAgedBrieOrBackstagePasses(updatedItem);
-      } else if (updatedItem.quality > 0) {
-        updatedItem.quality = updatedItem.quality - 1;
-      }
-
-      if (updatedItem.sellIn == 0) {
-        if (updatedItem.name != BRIE) {
-          if (updatedItem.name != PASSES) {
-            if (updatedItem.quality > 0) {
-              updatedItem.quality = updatedItem.quality - 1;
+        if (item.sellIn == 0) {
+          if (item.name != BRIE) {
+            if (item.name != PASSES) {
+              if (item.quality > 0) {
+                item.quality = item.quality - 1;
+              }
+            } else {
+              item.quality = 0;
             }
-          } else {
-            updatedItem.quality = 0;
           }
         }
-      }
 
-      return updatedItem;
-    });
+        return item;
+      });
 
     return new Shop(updatedItems);
   }
