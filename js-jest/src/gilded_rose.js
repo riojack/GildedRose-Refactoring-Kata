b683ad950f83c95349bf2612a77b14486fbd18e1
riojack <ayjay122@gmail.com>
2019-11-26 14:48:17 -0600

    refactor: pull string constants into a seaprate file

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,6 +1,4 @@
-const SULFURAS = 'Sulfuras, Hand of Ragnaros';
-const BRIE = 'Aged Brie';
-const PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+const {SULFURAS, BRIE, PASSES} = require('./item_names');
 
 const updateAgedBrieItem = (item) => {
   const {quality} = item;
---
+++ js-jest/src/item_names.js
@@ -0,0 +1,7 @@
+const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+const BRIE = 'Aged Brie';
+const PASSES = 'Backstage passes to a TAFKAL80ETC concert';
+
+module.exports = {
+  SULFURAS, BRIE, PASSES
+};
