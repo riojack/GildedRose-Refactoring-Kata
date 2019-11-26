0f1c4ad0b5897c8cc64f70e7a4b6a25bb3a639f0
riojack <ayjay122@gmail.com>
2019-11-26 13:57:34 -0600

    refactor: convert if to else-if to avoid unnecessary code

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -41,9 +41,7 @@ const updateItem = (item) => {
 
   if (item.sellIn == 0 && item.name != BRIE && item.name != PASSES && item.quality > 0) {
     item.quality = item.quality - 1;
-  }
-
-  if (item.sellIn == 0 && item.name == PASSES) {
+  } else if (item.sellIn == 0 && item.name == PASSES) {
     item.quality = 0;
   }
 
